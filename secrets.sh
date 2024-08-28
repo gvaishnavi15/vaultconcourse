931bc911cc935540aedfd70a56bde4b1eeaf2082
gvaishnavi15 <gvaishnavi159@gmail.com>
2024-08-28 10:56:02 +0530

    Update secrets.sh

--- secrets.sh
+++ secrets.sh
@@ -1,19 +1,25 @@
-#!/bin/bash
-
-# selecting authentication method as userpass by default it is token
 export VAULT_ADDR='http://192.168.3.54:8200'
-export VAULT_TOKEN=test
-# vault login -method=userpass username=vaishnavi password=vaishnavi
+export VAULT_TOKEN='myroot'
 
-# retrieving secrets from the vault
-username=$(vault kv get -field=username mysecrets/path)
-password=$(vault kv get -field=password mysecrets/path)
+EMAIL=$(vault kv get -field=userid screener/path)
+PASSWORD=$(vault kv get -field=password screener/path)
+PG_USER=$(vault kv get -field=PG_USER screener/path)
+PG_PASS=$(vault kv get -field=PG_PASS screener/path)
+PG_DB=$(vault kv get -field=PG_DB screener/path)
 
-screener_username=$(vault kv get -field=username screener/path)
-screener_password=$(vault kv get -field=password screener/path)
+mkdir -p credentials
+chmod +x credentials
+echo "$EMAIL" > credentials/email.txt
+echo "$PASSWORD" > credentials/password.txt
+echo "$PG_USER" > credentials/pg_user.txt
+echo "$PG_PASS" > credentials/pg_pass.txt
+echo "$PG_DB" > credentials/pg_db.txt
 
-# reading and printing userpass
-echo "Retrived Username: $username"
-echo "Retrived Password: $password"
-echo "Retrived Screener Username: $screener_username"
-echo "Retrived Screener Password: $screener_password"
+# Print the credentials for debugging (ensure this is secure and not used in production)
+echo "Fetched Email: $EMAIL"
+echo "Fetched Password: $PASSWORD"
+echo "Fetched PG_USER: $PG_USER"
+echo "Fetched PG_PASS: $PG_PASS"
+echo "Fetched PG_DB: $PG_DB"
+
+ls -l credentials
